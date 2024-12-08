f433368926655e3d4c9aeb322871a21fce8e710e
Danilo Birbiglia <danilobirbiglia@gmail.com>
2024-12-08 14:33:09 +0100

    Restructure for better organization

--- main.py
+++
@@ -1,7 +0,0 @@
-# Welcome to Any2YOLO
-import tkinter as tk
-from gui import Any2YOLOApp
-
-if __name__ == "__main__":
-    app = Any2YOLOApp()  
-    app.root.mainloop()
---
+++ src/main.py
@@ -0,0 +1,7 @@
+# Welcome to Any2YOLO
+import tkinter as tk
+from gui import Any2YOLOApp
+
+if __name__ == "__main__":
+    app = Any2YOLOApp()  
+    app.root.mainloop()
